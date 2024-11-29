cd237ec2687cca952ce70b5c4339febeee5854cb
emilykbrown <disabledsarcasm@gmail.com>
2024-11-28 22:36:53 -0600

    Starting building menu

---
+++ src/finalproject/Menu.java
@@ -0,0 +1,14 @@
+package finalproject;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		System.out.println("--Creature Management");
+		Scanner myScanner = new Scanner(System.in); // Create a Scanner Object
+		
+	}	
+	
+}
